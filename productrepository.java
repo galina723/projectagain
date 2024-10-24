b00006e42ca302afc9ea748fac0e6cf7821279b7
hoanggyen231 <147621826+galina723@users.noreply.github.com>
2024-10-24 13:09:32 +0700

    Revert "addmybranch"
    This reverts commit e1966002d47bd0cf01f145390d5830ee2714b7eb.

--- productrepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.products;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface productrepository extends CrudRepository<products, Integer> {
-    
-}
